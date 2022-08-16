6299b004a7e32c3f7becb8012608b1b49c0d0d08
Fidann12 <fidan.aliyeva03@icloud.com>
2022-08-16 10:02:12 +0400

    DONE

--- task-2/main.js
+++ task-2/main.js
@@ -9,7 +9,7 @@ prompt.get('input', function (err, result) {
     let x = parseInt(number[0]);
     let a = parseInt(number[1]);
     let b = parseInt(number[2]);
-    if (x <= a && a <= b ){
+    if (x < a && a < b ){
         console.log("OUT");
     }else{
         console.log("IN");
